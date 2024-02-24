f5ce68f1fdca92a9c678a5771fa8f2103e7b75f8
dany <dacr63@gmail.com>
2024-02-24 12:14:22 +0100

    eslint, prettier, husky cfg

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,5 @@
+/** @type {import('lint-staged').Config} */
+module.exports = {
+  "*.{js,cjs,ts,jsx,tsx,astro}": ["eslint --fix", "prettier --write"],
+  "*.{md,html,json,yaml,yml}": ["prettier --write"],
+};
