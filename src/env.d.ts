e48c96076e2b0dba4c4ceecdce5daa6b5ab37f1d
dany <dacr63@gmail.com>
2024-02-24 23:51:14 +0100

    fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-declare module 'typewriter-effect/dist/core' {
-	const Typewriter: typeof import('typewriter-effect').TypewriterClass
-	export default Typewriter
-}
+declare module 'typewriter-effect/dist/core'
