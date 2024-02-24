e16217acec46867298a5ef5487f965ffc28b34c9
dany <dacr63@gmail.com>
2024-02-24 16:07:25 +0100

    light/dark mode logic

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: true,
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					bg: 'rgb(var(--primary-bg) / <alpha-value>)',
+					text: 'rgb(var(--primary-text) / <alpha-value>)'
+				},
+				accent: 'rgb(var(--accent) / <alpha-value>)'
+			}
+		}
 	},
-	plugins: [],
+	plugins: []
 }
