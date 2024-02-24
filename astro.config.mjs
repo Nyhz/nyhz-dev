036f6f60148b968b63e33aee19e841dd48a0a2b5
dany <dacr63@gmail.com>
2024-02-24 23:32:36 +0100

    feat: new animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import partytown from '@astrojs/partytown'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: vercel()
-});
+	integrations: [
+		tailwind(),
+		partytown({
+			config: {
+				forward: ['dataLayer.push']
+			}
+		})
+	],
+	output: 'server',
+	adapter: vercel()
+})
